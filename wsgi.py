2996df4bc01a65835f63687bd4054eabdab185b0
potatorich14 <129529666+potatorich14@users.noreply.github.com>
2026-08-28 08:37:04 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,3 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 # Импортируем приложение
 from server.main import app as application
-
-# Инициализируем базу данных при запуске
-from server.main import init_db
-init_db()
